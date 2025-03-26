cb575f2f1f54fac0fec6695fb435251647104dc2
drain31 <draintoram@gmail.com>
2025-03-26 11:24:30 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Web3DLogin from './components/Web3DLogin'; // Mengimpor komponen Web3DLogin
+
+function App() {
+  return (
+    <div>
+      <Web3DLogin /> {/* Memanggil komponen Web3DLogin */}
+    </div>
+  );
+}
+
+export default App;
